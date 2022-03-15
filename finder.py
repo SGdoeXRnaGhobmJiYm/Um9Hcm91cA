7211d3e3af90a423078aa3fd739cc42f09328840
zaxhodV2 <88345989+zaxhodV2@users.noreply.github.com>
2022-03-15 22:50:07 +0600

    Update finder.py

--- finder.py
+++ finder.py
@@ -1,9 +1,12 @@
 from lib.controllers import Controller
 from lib.arguments import parse_args
-import multiprocessing, requests, time
+from colorama import Fore
+import multiprocessing, requests, time, pyfiglet
 fe = open("proxies.txt", "a")
 be = open("proxies.txt", "r")
-
+txt = pyfiglet.figlet_format(text="Sprites Tools!")
+print(f' {Fore.LIGHT_GREEN_EX}{txt}\n')
+print(f' {Fore.RED}[+] Loading Program...\n')
 def scrape():
   global fe, be
   be.truncate(0)

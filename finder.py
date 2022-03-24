8e2fbc45e55d6df2439f8793da025b2fc925692a
zaxhodV2 <88345989+zaxhodV2@users.noreply.github.com>
2022-03-24 21:20:15 +0600

    Update finder.py

--- finder.py
+++ finder.py
@@ -7,18 +7,8 @@ be = open("proxies.txt", "w")
 txt = pyfiglet.figlet_format(text="Sprites Tools!")
 print(f' {Fore.LIGHTGREEN_EX}{txt}\n')
 print(f' {Fore.RED}[+] Loading Program...\n')
-def scrape():
-  global fe, be
-  be.truncate(0)
-  be.close()
-  r = requests.get('https://raw.githubusercontent.com/UserR3X/proxy-list/main/online/http%2Bs.txt')
-  x = r.text
-  fe.write(x)
-  fe.close()
 
 if __name__ == "__main__":
-    scrape()
-    time.sleep(1)
     multiprocessing.freeze_support()
     controller = Controller(
         arguments=parse_args()

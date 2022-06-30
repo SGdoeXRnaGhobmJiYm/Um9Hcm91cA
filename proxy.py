5d18a79128d3cb73529dca1a4a550270c43e27b4
SGdoeXRnaGhobmJiYm <88345989+SGdoeXRnaGhobmJiYm@users.noreply.github.com>
2022-06-30 23:28:07 +0600

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,6 +1,14 @@
+import os
+import threading
+import urllib.request
+import socket
+import re
+import random
+import json
 
-import urllib.request, json
 
+alive = 0
+file = open("proxy.txt", "a")
 
 useragents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36",
@@ -25,10 +33,62 @@ def get(url):
     text = urllib.request.urlopen(req, timeout=10).read().decode('utf-8')
     return text
 
+def github():
+    urls = [
+        "raw.githubusercontent.com/almroot/proxylist/master/list.txt",
+        
+        "raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
+        
+        "raw.githubusercontent.com/hendrikbgr/Free-Proxy-Repo/master/proxy_list.txt",
+        
+        "raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
+        
+        "raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies.txt",
+        
+        "raw.githubusercontent.com/KUTlime/ProxyList/main/ProxyList.txt",
+        
+        #"raw.githubusercontent.com/manuGMG/proxy-365/main/SOCKS5.txt",
+
+        "raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt",
+        
+        "raw.githubusercontent.com/mmpx12/proxy-list/master/proxies.txt",
+
+        "raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/http.txt",
+        
+        #"raw.githubusercontent.com/ryanhaticus/superiorproxy.com/main/proxies.txt",
+
+        "raw.githubusercontent.com/saschazesiger/Free-Proxies/master/proxies/ultrafast.txt",
+
+        "raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
+        "raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt",
+        #"raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt",
+        #"raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt",
+        
+        "raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt",
+
+        "raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+
+        "raw.githubusercontent.com/Volodichev/proxy-list/main/http.txt",
+        "raw.githubusercontent.com/Volodichev/proxy-list/main/hproxy.txt",
+        
+        "raw.githubusercontent.com/zeynoxwashere/proxy-list/main/http.txt"
+    ]
+    resp = []
+
+    for url in urls:
+        try:
+            for x in re.findall(r"\d+\.\d+\.\d+\.\d+\:\d+", get(url)):
+                resp.append(x)
+        except Exception as e:
+            pass
+
+    return list(set(resp))
+
 def checkerproxy_net():
     urls = [
         "checkerproxy.net/api/archive"
     ]
+    resp = []
 
     for url in urls:
         #try:
@@ -42,16 +102,123 @@ def checkerproxy_net():
                     proxies = json.loads(proxies)
                 
                     for proxy in proxies:
-                        if proxy["type"] == 1 or 2 and proxy["kind"] == 2 and proxy["timeout"] <= 500:
-                          with open('proxy.txt', 'a') as file:
-                            file.write(proxy["addr"] + '\n'))
-                            file.close()
+                        resp.append(proxy["addr"])
                 except:
                     print("Failed")
-            return True
+                
+        #except:
+            #print("Failed")
 
-open("proxy.txt", "w")
-checkerproxy_net()
-with open("proxy.txt", "a") a fp:
-  total = fp.readlines()
-print(f"Total Proxy: {len(total)}")
+    return list(set(resp))
+
+def openproxy_space():
+    urls = [
+        "openproxy.spce/list/http"
+    ]
+    resp = []
+
+    for url in urls:
+        try:
+            for x in re.findall(r"\d+\.\d+\.\d+\.\d+\:\d+", get(url)):
+                resp.append(x)
+        except Exception as e:
+            pass
+
+    return list(set(resp))
+
+def spys_me():
+    urls = [
+        "spys.me/proxy.txt"
+    ]
+    resp = []
+
+    for url in urls:
+        try:
+            for x in re.findall(r"\d+\.\d+\.\d+\.\d+\:\d+", get(url)):
+                resp.append(x)
+        except Exception as e:
+            pass
+
+    return list(set(resp))
+
+def free_proxy_list_net():
+    urls = [
+        "free-proxy-list.net/",
+        "www.us-proxy.org/",
+        "free-proxy-list.net/uk-proxy.html",
+        "www.sslproxies.org/",
+        "free-proxy-list.net/anonymous-proxy.html",
+        "www.google-proxy.net/"
+    ]
+    resp = []
+
+    for url in urls:
+        try:
+            for x in re.findall(r"\d+\.\d+\.\d+\.\d+\:\d+", get(url)):
+                resp.append(x)
+        except Exception as e:
+            pass
+
+    return list(set(resp))
+    
+def check(proxy):
+    global alive, file
+    headers = """GET / HTTP/1.1\r\n
+Host: google.com.tr\r\n\r\n"""
+
+    ip = proxy.split(":")[0]
+    port = int(proxy.split(":")[1])
+
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ip,port))
+        sock.send(headers.encode('utf-8'))
+        resp = s.recv(3000).decode('utf-8')
+        file.write(proxy + "\n")
+        print(f"Good Proxy: {proxy}")
+        alive += 1
+        s.close()
+    except socket.error as m:
+       print(f"Bad Proxy: {proxy}")
+       s.close()
+
+
+def checker(proxies):
+    threads = []
+    try:
+        for proxy in proxies:
+            th = threading.Thread(target=check, args=[proxy])
+            th.start()
+            threads.append(th)
+    except:
+        pass
+
+    for th in threads:
+        th.join()
+    return threads 
+
+if __name__ == "__main__":
+    os.system("clear")
+    open("proxy.txt", "w")
+    proxies = []
+    for x in github():
+      proxies.append(x)
+    
+    for x in checkerproxy_net():
+      proxies.append(x)
+
+    for x in openproxy_space():
+      proxies.append(x)
+
+    for x in spys_me():
+      proxies.append(x)
+
+    for x in free_proxy_list_net():
+      proxies.append(x)
+    print(f"Total Scraped: {len(proxies)}")    
+    proxies = re.findall(r"\d+\.\d+\.\d+\.\d+\:\d+", str(proxies))
+    proxies = set(proxies)
+    proxies = sorted(proxies)
+    checker(proxies)
+    print(f"\nTotal Working: {alive}")

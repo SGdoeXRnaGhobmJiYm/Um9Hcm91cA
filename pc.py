26db4ab337c42746a240a26bfa8ff9bfc701562b
SGdoeXRnaGhobmJiYm <88345989+SGdoeXRnaGhobmJiYm@users.noreply.github.com>
2022-07-01 14:32:26 +0600

    Create pc.py

---
+++ pc.py
@@ -0,0 +1,62 @@
+import random
+import urllib.request, json
+
+
+useragents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/99.0.4844.59 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (iPad; CPU OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/99.0.4844.59 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; SM-N960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; LM-Q720) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; LM-X420) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; LM-Q710(FGN)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36"
+]
+
+def get(url):
+    req = urllib.request.Request(f"https://{url}", headers={'User-Agent': random.choice(useragents)})
+    text = urllib.request.urlopen(req, timeout=10).read().decode('utf-8')
+    return text
+
+def checkerproxy_net():
+    urls = [
+        "checkerproxy.net/api/archive"
+    ]
+
+    for url in urls:
+        #try:
+            req = get(url)
+            archives = json.loads(req)
+            
+            for archive in archives:
+                try:
+                    date = archive["date"]
+                    proxies = get(f"{url}/{date}")
+                    proxies = json.loads(proxies)
+                
+                    for proxy in proxies:
+                        if proxy["type"] == 4 and proxy["kind"] == 2 and proxy["timeout"] <= 10000:
+                          with open('proxy.txt', 'a') as file:
+                            file.write(proxy["addr"] + '\n'))
+                            file.close()
+                except:
+                    print("Failed")
+            return True
+
+open("proxy.txt", "w")
+checkerproxy_net()
+with open('proxy.txt') as result:
+        uniqlines = set(result.readlines())
+with open('proxy.txt', 'w') as rmdup:
+        rmdup.writelines(set(uniqlines))
+with open('proxy.txt') as r:
+        u = sorted(r.readlines())
+with open('proxy.txt', 'w') as dp:
+        dp.writelines(sorted(u))
